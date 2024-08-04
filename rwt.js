9d626a885fce1541ace4ae7c7e02562fce5aa586
Mariama Balde <foulah18@gmail.com>
2024-08-04 14:28:01 +0000

    Faire la modification d'un todo list(Atelier 1)

---
+++ rwt.js
@@ -0,0 +1,82 @@
+class Todo extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            input: "",
+            todos: [],
+            editIndex: null,
+            editInput: ""
+        }
+    }
+
+    render() {
+        return (
+            <div className="container col-lg-6 offset-lg-6 col-12 m-auto mt-5">
+                <div className="input-group mb-3">
+                    <input type="text"
+                        value={this.state.input}
+                        onChange={(e) => {
+                            this.setState({ input: e.target.value })
+                        }}
+                        className="form-control"
+                        placeholder="Ajouter une tache" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <button className="btn btn-outline-primary"
+                        onClick={() => {
+                            this.setState({ todos: [...this.state.todos, this.state.input] })
+                        }}
+                        id="button-addon2">Ajouter</button>
+                </div>
+                <div className="list-group">
+                    <ul class="list-group" >
+                        {this.state.todos.map((todo, index) => {
+                            return <li key={index} class="list-group-item d-flex justify-content-between align-items-start">
+                                {this.state.editIndex === index ? (
+                                    <div className="ms-2 me-auto d-flex">
+                                        <input type="text"
+                                            value={this.state.editInput}
+                                            onChange={(e) => {
+                                                this.setState({ editInput: e.target.value })
+                                            }}
+                                            className="form-control" />
+                                        <button class="btn btn-sm btn-outline-success"
+                                            onClick={() => {
+                                                const newTodos = [...this.state.todos];
+                                                newTodos[index] = this.state.editInput;
+                                                this.setState({
+                                                    todos: newTodos,
+                                                    editIndex: null,
+                                                    editInput: ""
+                                                })
+                                            }}
+                                        >Enregistrer</button>
+                                    </div>
+                                ) : (
+                                    <div class="ms-2 me-auto">
+                                        {todo}
+                                    </div>
+                                )}
+                                <i className="fa-solid fa-pen-to-square me-3"
+                                    onClick={() => {
+                                        this.setState({
+                                            editIndex: index,
+                                            editInput: this.state.todos[index]
+                                        })
+                                    }}
+                                    style={{ color: "#FFD43B" }}>
+                                </i>
+                                <i className="fa-solid fa-trash"
+                                    onClick={() => {
+                                        //filtrer le tableau en fonction de l'index des éléments
+                                        this.setState({ todos: this.state.todos.filter((element, i) => i !== index) });
+                                    }}
+                                    style={{ color: "#ed0202" }}></i>
+                            </li>
+                        })}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Todo />, document.getElementById("root"));
